6a83e6f84ef4783cd488d7e8ac2329ae97337c0c
Leila Bekturgan <leila.bekturgan@gmail.com>
2023-07-07 14:35:38 +0600

    carousel at my stories page

--- app/stories/stories.jsx
+++ app/stories/stories.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+'use client'
+import { useState } from 'react';
 import Story from './story';
+import Link from 'next/link';
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+const slides = [
+  {"title": "Story 1", "link": "/chat"},
+  {"title": "Story 2", "link": "/chat"},
+  {"title": "Story 3", "link": "/chat"},
+  {"title": "Story 4", "link": "/chat"},
+  {"title": "Story 5", "link": "/chat"},
+  {"title": "Story 6", "link": "/chat"},
+  {"title": "Story 7", "link": "/chat"},
+];
 
 const Stories = () => {
+  const [selectedSlide, setSelectedSlide] = useState(0);
+
+  const handlePreviousSlide = () => {
+    setSelectedSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+  };
+
+  const handleNextSlide = () => {
+    setSelectedSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+  };
+
   return (
-    <div className="wrapper absolute inset-0 overflow-auto">
-      <div className="z-10 flex flex-wrap">
-        <Story imageUrl="dobby.jpg" title="Create story" link="/chat" />
+    <div className="wrapper absolute inset-0 overflow-hidden">
+      <div className="z-10 flex flex-wrap justify-center items-center">
+        <Carousel selectedItem={selectedSlide} showArrows={false} showThumbs={false}>
+          {slides.map((story) => (
+            <Story
+              key={story.title}
+              title={story.title}
+              link={story.link}
+              text='asdsdasdasdasdasdasdasdasdasdasdasdasdasd asdasdasdasdas asd'
+            />
+          ))}
+        </Carousel>
+        <div className="absolute top-1/2 left-2">
+          <button onClick={handlePreviousSlide} className="bg-gray-200 px-3 py-2 rounded-full text-lg">
+            &lt;
+          </button>
+        </div>
+        <div className="absolute top-1/2 right-2">
+          <button onClick={handleNextSlide} className="bg-gray-200 px-3 py-2 rounded-full text-lg">
+            &gt;
+          </button>
+        </div>
       </div>
     </div>
-
   );
 };
 
--- app/stories/story.jsx
+++ app/stories/story.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Story = ({ title, link }) => {
-  const unfinishedText = "This is an unfinished story.";
-
+const Story = ({ title, link, text }) => {
   return (
-    <div className="max-w-xs mx-auto rounded-lg shadow-lg overflow-hidden mt-48 p-10"  style={{ backgroundImage: "url('/cardbg.jpg')" }}>
+    <div className="max-w-xs mx-auto rounded-lg shadow-lg overflow-hidden mt-48 p-10 h-80 w-80" style={{ backgroundImage: "url('/oldbook.jpg')", backgroundPosition: "center" }}>
       <h2 className="text-xl font-semibold mb-2">{title}</h2>
-      <p className="text-gray-500 mb-4">
-        <span className="text-2xl font-bold text-blue-500">{unfinishedText.charAt(0)}</span>
-        {unfinishedText.slice(1)}
+      <p className="text-gray-500 mb-4" style={{ wordBreak: 'break-word' }}>
+        <span className="text-2xl font-bold text-blue-500">{text.charAt(0)}</span>
+        {text.slice(1)}
       </p>
-      <Link href={link}>
+      <a href={link}>
         <p className="text-blue-500 font-semibold hover:underline">Read More</p>
-      </Link>
+      </a>
     </div>
   );
 };
+
+
+
 
 export default Story;
